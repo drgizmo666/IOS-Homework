1a56fb738ff14208e1a2f083d6c17f9a5e559d08
drgizmo666 <student@cit132mac18.icl.lanecc.edu>
2016-02-17 12:27:13 -0800

    lab 2
    Added lab 2

---
+++ Lab2/tictactoe/tictactoe/tictactoe/tictactoe.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+namespace tictactoe
+{
+	public class tictactoe
+	{
+		public string[,] board = new string[3,3];
+
+		public tictactoe ()
+		{
+		}
+			
+		public static bool IsValid(string input)
+		{
+			string validInput = input.ToUpper ();
+			if (validInput == "X") 
+			{
+				return true;
+			} else 
+			{
+				return false;
+			}
+		}
+
+		public static string FormatInput(string input)
+		{
+			return input.ToUpper ();
+		}
+
+		public static int ComputerMove()
+		{
+			Random r = new Random ();
+			int num;
+			num = r.Next (9);
+			return num;
+		}
+			
+	}
+}
+
