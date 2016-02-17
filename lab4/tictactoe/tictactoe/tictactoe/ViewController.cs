f9d2ee381c53ebf30fe1039f8341b16d2a867b62
drgizmo666 <student@cit132mac18.icl.lanecc.edu>
2016-02-17 13:30:01 -0800

    lab 4
    added late 4

---
+++ lab4/tictactoe/tictactoe/tictactoe/ViewController.cs
@@ -0,0 +1,246 @@
+﻿using System;
+
+using UIKit;
+
+namespace tictactoe
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void CompMove_TouchUpInside (UIButton sender)
+		{
+			if(box1.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box1.Text) == true){
+					box1.Text = tictactoe.FormatInput(box1.Text);
+					box1.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box2.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box2.Text) == true){
+					box2.Text = tictactoe.FormatInput(box2.Text);
+					box2.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box3.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box3.Text) == true){
+					box3.Text = tictactoe.FormatInput(box3.Text);
+					box3.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box4.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box4.Text) == true){
+					box4.Text = tictactoe.FormatInput(box4.Text);
+					box4.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box5.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box5.Text) == true){
+					box5.Text = tictactoe.FormatInput(box5.Text);
+					box5.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box6.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box6.Text) == true){
+					box6.Text = tictactoe.FormatInput(box6.Text);
+					box6.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box7.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box7.Text) == true){
+					box7.Text = tictactoe.FormatInput(box7.Text);
+					box7.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box8.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box8.Text) == true){
+					box8.Text = tictactoe.FormatInput(box8.Text);
+					box8.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+			else if(box9.IsFirstResponder)
+			{
+				if(tictactoe.IsValid(box9.Text) == true){
+					box9.Text = tictactoe.FormatInput(box9.Text);
+					box9.Enabled = false;
+					PlaceComputer();
+				}
+				else
+				{
+					var alert = UIAlertController.Create ("Not supported", "Please Input an X", UIAlertControllerStyle.Alert);
+					alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController (alert, true, null);
+				}
+			}
+		}
+
+		public void PlaceComputer()
+		{
+			int Move = 1;
+			while (Move == 1) {
+				switch (tictactoe.ComputerMove ()) {
+				case 1:
+					if (box1.Text == "") {
+						box1.Text = "O";
+						box1.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove();
+					}
+					break;
+				case 2:
+					if (box2.Text == "") {
+						box2.Text = "O";
+						box2.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 3:
+					if (box3.Text == "") {
+						box3.Text = "O";
+						box3.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 4:
+					if (box4.Text == "") {
+						box4.Text = "O";
+						box4.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 5:
+					if (box5.Text == "") {
+						box5.Text = "O";
+						box5.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 6:
+					if (box6.Text == "") {
+						box6.Text = "O";
+						box6.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 7:
+					if (box7.Text == "") {
+						box7.Text = "O";
+						box7.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 8:
+					if (box8.Text == "") {
+						box8.Text = "O";
+						box8.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+				case 9:
+					if (box9.Text == "") {
+						box9.Text = "O";
+						box9.Enabled = false;
+						Move = 0;
+					} else {
+						tictactoe.ComputerMove ();
+					}
+					break;
+
+				}
+			}
+		}
+	}
+}
---
+++ lab4/tictactoe/tictactoe/tictactoe/ViewController.designer.cs
@@ -0,0 +1,110 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace tictactoe
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box3 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box4 { get; set; }
+
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box5 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box6 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box7 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box8 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField box9 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton compMove { get; set; }
+
+		[Action ("CompMove_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void CompMove_TouchUpInside (UIButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (box1 != null) {
+				box1.Dispose ();
+				box1 = null;
+			}
+			if (box2 != null) {
+				box2.Dispose ();
+				box2 = null;
+			}
+			if (box3 != null) {
+				box3.Dispose ();
+				box3 = null;
+			}
+			if (box4 != null) {
+				box4.Dispose ();
+				box4 = null;
+			}
+			if (box4 != null) {
+				box4.Dispose ();
+				box4 = null;
+			}
+			if (box5 != null) {
+				box5.Dispose ();
+				box5 = null;
+			}
+			if (box6 != null) {
+				box6.Dispose ();
+				box6 = null;
+			}
+			if (box7 != null) {
+				box7.Dispose ();
+				box7 = null;
+			}
+			if (box8 != null) {
+				box8.Dispose ();
+				box8 = null;
+			}
+			if (box9 != null) {
+				box9.Dispose ();
+				box9 = null;
+			}
+			if (compMove != null) {
+				compMove.Dispose ();
+				compMove = null;
+			}
+		}
+	}
+}
